895be6e63d2f8db1e33aa3c876578b994c2dc264
Wojciech Jarosz <jaroszw@gmail.com>
2020-11-22 20:38:07 +0100

    initial

---
+++ src/components/BookList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class BookList extends Component {
+  static contextType = ThemeContext;
+
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <div
+        className="book-list"
+        style={{ background: theme.ui, color: theme.syntax }}
+      >
+        <ul>
+          <li>the way of kings</li>
+          <li>the name of the wind</li>
+          <li>the final empire</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
